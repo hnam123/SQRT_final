09809a6dde2bb2aa2979b9969c15a403194e8f0d
hnam123 <105033505+hnam123@users.noreply.github.com>
2026-05-26 07:45:53 +0700

    Add files via upload

---
+++ src/sim/SQRT_tb.vhd
@@ -0,0 +1,64 @@
+LIBRARY IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.Mylib.all;
+
+ENTITY SQRT_tb IS
+END SQRT_tb;
+
+ARCHITECTURE bev OF SQRT_tb IS
+ Constant DATA_WIDTH : integer := 16;
+ SIGNAL RST, CLK :  STD_LOGIC := '0';
+ SIGNAL	Start_i:  STD_LOGIC;
+ SIGNAL	a_i:  STD_LOGIC_VECTOR(DATA_WIDTH -  1 downto 0);
+ SIGNAL	SQRT_o: 	 STD_LOGIC_VECTOR(DATA_WIDTH -  1 downto 0);
+ SIGNAL	Done_o :  STD_LOGIC;
+BEGIN
+
+UUT: SQRT
+  GENERIC MAP (DATA_WIDTH)
+ PORT MAP(CLK,RST, Start_i ,
+	a_i ,
+	Done_o ,
+	Sqrt_o
+);
+CLK <= not CLK  after 10 ns;
+-- stimulus
+Stimulus: PROCESS
+ BEGIN
+--case 0
+  START_i <= '0';
+  RST <= '1'; wait for 20 ns;
+  RST <= '0'; wait for 20 ns;
+--case 1 a_i = 0
+  a_i <= "0000000000000000";
+  Start_i <= '1';
+  wait until Done_o = '1';
+  Start_i <= '0';
+  wait for 100 ns;
+--case 2 a_i = 0.25
+  a_i <= "0000000001000000";
+  Start_i <= '1';
+  wait until Done_o = '1';
+  Start_i <= '0';
+  wait for 100 ns;
+--case 3 a_i = 0.5
+  a_i <= "0000000010000000";
+  Start_i <= '1';
+  wait until Done_o = '1';
+  Start_i <= '0';
+  wait for 100 ns;
+--case 4 a_i = 1
+  a_i <= "0000000100000000";
+  Start_i <= '1';
+  wait until Done_o = '1';
+  Start_i <= '0';
+  wait for 100 ns;
+--case 5 a_i = 4
+  a_i <= "0000010000000000";
+  Start_i <= '1';
+  wait until Done_o = '1';
+  Start_i <= '0';
+  wait for 100 ns;
+END PROCESS;
+END BEV;
